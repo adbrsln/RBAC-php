d957c99544559d790f2e46170bfdd767f65d3c51
adbroslan <adbroslan@gmail.com>
2018-04-02 17:08:13 +0800

    reupdate the sql remove the accountStatus

--- rbac.sql
+++ rbac.sql
@@ -4,14 +4,13 @@ CREATE TABLE `user` (
 	`password` VARCHAR(255) NULL DEFAULT NULL,
 	`name` VARCHAR(50) NULL DEFAULT NULL,
 	`userType` INT(10) NULL DEFAULT NULL,
-	`accountStatus` INT(11) NULL DEFAULT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `id` (`id`)
 )
-INSERT INTO `user` (`id`, `email`, `password`, `name`, `userType`, `accountStatus`) VALUES
-	(57, 'admin@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 1, 1),
-	(58, 'user@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 2, 1),
-	(59, 'accountant@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 3, 1);
+INSERT INTO `user` (`id`, `email`, `password`, `name`, `userType`) VALUES
+	(57, 'admin@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 1),
+	(58, 'user@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 2),
+	(59, 'accountant@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 3);
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=57
