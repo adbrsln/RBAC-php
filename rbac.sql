ce6f93824489ec00eb100a34d07e9a6b5aa8572b
adbroslan <adbroslan@gmail.com>
2018-04-02 16:35:36 +0800

    updated sql to insert user

--- rbac.sql
+++ rbac.sql
@@ -8,6 +8,10 @@ CREATE TABLE `user` (
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `id` (`id`)
 )
+INSERT INTO `user` (`id`, `email`, `password`, `name`, `userType`, `accountStatus`) VALUES
+	(57, 'admin@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 1, 1),
+	(58, 'user@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 2, 1),
+	(59, 'accountant@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'Muhammad Farid', 3, 1);
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=57
